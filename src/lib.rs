4af6a71a6ee5c9c5c3d52820645d206a83dde3e8
Nicolas Chan <nicolas@nicolaschan.com>
2020-11-29 22:33:11 -0800

    Satisfy clippy

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,10 @@ use self::diesel::prelude::*;
 pub mod schema;
 pub mod models;
 
-pub fn create_hit(conn: &SqliteConnection, ip_addr: &String, timestamp: std::time::Duration) -> usize {
+pub fn create_hit(conn: &SqliteConnection, ip_addr: &str, timestamp: std::time::Duration) -> usize {
     use schema::hits;
     let new_hit = NewHit {
-        ip_addr: ip_addr.clone(),
+        ip_addr: ip_addr.to_string(),
         timestamp: timestamp.as_millis().to_string()
     };
     diesel::insert_into(hits::table)
